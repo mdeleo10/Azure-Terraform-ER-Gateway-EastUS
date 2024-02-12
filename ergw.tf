c7bde7e0f547b65329be564328698e19ceeea5f7
Michael De Leo <mdeleo10@hotmail.com>
2024-02-12 17:16:22 -0500

    Test

---
+++ ergw.tf
@@ -0,0 +1,46 @@
+# Terraform GitHub storage account for state output
+#
+#
+# Create Storage Account Container
+# Suggestion: create tfstate-repo-name, needs to be lower case only
+#
+# az storage container create -n tftstate-azure-terraform-vpngw-connecitivity-hub-eastus --account-name  cloudmdterraformstate
+#
+terraform {
+  backend "azurerm" {
+    resource_group_name     = "rg-terraform-state-001"
+    storage_account_name    = "cloudmdterraformstate"
+    container_name          = "tftstate-azure-terraform-vpngw-connecitivity-hub-westus3"
+    key                     = "tfstate"
+  }
+}
+
+
+# Define Resource Group Name, including the Azure Region
+  resource "azurerm_resource_group" "rg" {
+    name      = "rg-${var.resource_group_name_prefix}-${var.resource_group_location}"
+    location  = var.resource_group_location
+    tags = {
+        Source = "terraform"
+        Environment = "development"
+        CreatedDate = timestamp()
+    }
+  }
+
+# Azure ExpressRoute Circuit
+resource azurerm_express_route_circuit "express_route" {
+    name                  = "er-${var.resource_group_name_prefix}-${var.resource_group_location}"
+    resource_group_name   = azurerm_resource_group.rg.name
+    location              = azurerm_resource_group.rg.location
+
+    # https://docs.microsoft.com/en-us/azure/expressroute/expressroute-locations-providers
+    service_provider_name = "Coresite"
+    peering_location      = "Washington DC"
+    bandwidth_in_mbps     = 50
+
+    sku {
+        tier   = "Standard"  
+        family = "MeteredData" 
+    }
+}
+
---
+++ variables.tf
@@ -0,0 +1,10 @@
+
+variable "resource_group_name_prefix" {
+  default       = "Connectivity-Hub"
+  description   = "Prefix of the resource group name that's combined with a region"
+}
+
+variable "resource_group_location" {
+  default       = "eastus"
+  description   = "Location of the resource group."
+}
